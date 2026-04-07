d40e47924cf90ddbcd62eb2b6f2e63cb2578acda
Aya <ayaaa.okzk@gmail.com>
2026-04-07 14:09:29 -0700

    Add Post views and serializers

--- apps/content/serializers.py
+++ apps/content/serializers.py
@@ -1,16 +1,19 @@
 # DRF serializers for Pages, Posts, Attachments.
 # To be implemented in Week 3.
 from rest_framework import serializers
-from .models import Page, Attachment
+
+from .models import Attachment, Page, Post
 
 
+## -- Pages -- ##
+# This set the attachment for PageSerializer to get the attachments list
 class AttachmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Attachment
         fields = ['title', 'file', 'display_order']
 
 class PageSerializer(serializers.ModelSerializer):
-    attachments = AttachmentSerializer(many = True)
+    attachments = AttachmentSerializer(many = True, read_only = True)
     class Meta:
         model = Page
         fields = ['title', 'slug','body','attachments']
@@ -20,3 +23,18 @@ class PageListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Page
         fields = ['title', 'slug', 'display_order']
+
+
+## -- Posts -- ##
+class PostSerializer(serializers.ModelSerializer):
+    attachments = AttachmentSerializer(many = True, read_only = True)
+    class Meta:
+        model = Post
+        fields = ['title', 'slug','body','attachments']
+        
+        
+class PostListSerializer(serializers.ModelSerializer):
+    attachments = AttachmentSerializer(many = True ,read_only = True)
+    class Meta:
+        model = Post
+        fields = ['title', 'slug', 'body', 'published_at', 'attachments']
--- apps/content/views.py
+++ apps/content/views.py
@@ -1,9 +1,12 @@
 # API views for public read-only content endpoints.
 # To be implemented in Week 3.
 from rest_framework.generics import ListAPIView, RetrieveAPIView
-from .models import Page
-from .serializers import PageListSerializer, PageSerializer
 
+from .models import Page, Post
+from .serializers import PageListSerializer, PageSerializer, PostListSerializer, PostSerializer
+
+
+## -- Pages -- ##
 # This endpoint is get one page by it's slug.
 class PageView(RetrieveAPIView):
   serializer_class = PageSerializer
@@ -13,11 +16,25 @@ class PageView(RetrieveAPIView):
     return Page.objects.filter(status='published')
   
   
-  
-
 # This endpoint is list all published pages 
 class PageListView(ListAPIView):
     serializer_class = PageListSerializer
 
     def get_queryset(self):
-        return Page.objects.filter(status='published').order_by('display_order', 'title')
+        return Page.objects.filter(status='published')
+      
+      
+## -- Posts -- ##
+class PostView(RetrieveAPIView):
+  serializer_class = PostSerializer
+  lookup_field = 'slug'
+  
+  def get_queryset(self):
+    return Post.objects.filter(status='published')
+  
+  
+class PostListView(ListAPIView):
+    serializer_class = (PostListSerializer)
+    
+    def get_queryset(self):
+      return Post.objects.filter(status='published')
